50ae9d4a1d9cec8cec6f6bc430275c2e3abeee44
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-03-11 17:49:35 +0300

    refactor: moved some logic from `Display`to`Field`

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -7,29 +7,20 @@ const Display = ({time, showBtn}) => {
   return (
     <main className='main'>
       <Field
-        time={
-            (time.h >= 10)
-            ? time.h 
-            : "0" + time.h
-          }
-          showBtn={showBtn} />
+        fullTime={time}
+        keyWord={"h"}
+        showBtn={showBtn} />
       <Spacer />
       <Field
-        time={
-            (time.m >= 10)
-            ? time.m 
-            : "0" + time.m
-          }
-          showBtn={showBtn} />
+        fullTime={time}
+        keyWord={"m"}
+        showBtn={showBtn} />
       
       <Spacer />
       <Field
-        time={
-            (time.s >= 10)
-            ? time.s 
-            : "0" + time.s
-          }
-          showBtn={showBtn} />
+        fullTime={time}
+        keyWord={"s"}
+        showBtn={showBtn} />
     </main>
   );
 };
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ButtonSet from './ButtonSet';
 
-const Field = ({time, showBtn}) => {
+const Field = ({fullTime, showBtn, keyWord}) => {
+  let time;
+  switch(keyWord) {
+    case 'h':
+      time = fullTime.h;
+    break
+    case 'm':
+      time = fullTime.m;
+    break
+    default:
+      time = fullTime.s;
+    break
+  }
+  console.log(time)
+  if (time < 10) time = "0" +time;
+  
   return (
     <div className='field'>
       {showBtn ? <ButtonSet /> : null}
